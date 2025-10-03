d3d1b0fd2082d3295318b66007cf181e4c19f309
Eldar1718 <eldarbek17@mail.ru>
2025-10-03 17:19:07 +0300

    ✅ Добавлена навигация между страницами ✅ Создана страница каталога игр (пока с заглушками) ✅ Сохранена существующая функциональность ✅ Использован существующий Bootstrap

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+# app/controllers/games_controller.rb
+class GamesController < ApplicationController
+  def index
+    # Пока заглушка - позже добавим реальные игры
+    @games = []
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# config/routes.rb
 Rails.application.routes.draw do
   root 'home#index'
+
+  # Добавляем маршрут для игр
+  get 'games', to: 'games#index'
+
+  # Devise маршруты (должны быть уже)
   devise_for :users
 end
